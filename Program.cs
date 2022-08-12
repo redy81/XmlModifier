084867c8d39776b6cdb9b2904f2ede50f4d14540
Filippo <88429953+redy81@users.noreply.github.com>
2022-08-12 12:46:18 +0200

    Added the files of the project

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Xml;
+
+namespace XmlModifier
+{
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "XmlModifier             v1.0            F.Branchetti" );
+
+            if (args.Length < 3)
+            {
+                Console.WriteLine( "Not enough parameters!" );
+                Environment.Exit( 1 );
+            }
+
+            if (!File.Exists( args[0] ))
+            {
+                Console.WriteLine( "Input file does not exist!" );
+                Environment.Exit( 2 );
+            }
+
+            string filePath = args[0];
+            string elementTag = args[1];
+            string newText = args[2];
+
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlNodeList xmlTargetNodes;
+
+            try
+            {
+                xmlDoc.Load( filePath );
+            }
+            catch (Exception)
+            {
+                Console.WriteLine( "Invalid XML structure!" );
+                Environment.Exit( 3 );
+                return; // Makes the compiler happy
+            }
+
+            // Find the path
+            try
+            {
+                xmlTargetNodes = xmlDoc.SelectNodes( elementTag );
+            }
+            catch (Exception)
+            {
+                Console.WriteLine( "Invalid Xpath!" );
+                Environment.Exit( 4 );
+                return; // Makes the compiler happy
+            }
+
+            Console.WriteLine( $"> Found {xmlTargetNodes.Count} nodes @ {elementTag}" );
+
+            foreach (XmlElement el in xmlTargetNodes)
+            {
+                Console.WriteLine( $"- {el.Name}" );
+                el.InnerText = newText;
+            }
+
+            try
+            {
+                File.Copy( filePath, filePath + ".bak" );
+
+                xmlDoc.Save( filePath );
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine( "Cannot save back to the file!" );
+                Environment.Exit( 5 );
+            }
+
+            Environment.Exit( 0 );
+        }
+    }
+}
